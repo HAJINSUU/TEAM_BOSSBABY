9d4d03b6500cb68723b7f2a98ef8cb4412b854ff
smhrd <smhrd@218.149.140.22>
2022-07-28 14:21:23 +0900

    [하진수] Review DTO 및 컨트롤러 내용 추가 _ 2022-07-28

--- WhereSming/model/com/wheresming/review/ReviewDTO.java
+++ WhereSming/model/com/wheresming/review/ReviewDTO.java
@@ -1,5 +1,14 @@
 package com.wheresming.review;
 
+import java.sql.Timestamp;
+
 public class ReviewDTO {
 
+	private int mv_seq;
+	private String cmt_content;
+	private Timestamp cmt_date;
+	private String mb_id;
+	private int cmt_likes;
+	private int cmt_score;
+	
 }
