f837395dcf0a31cc793c4c62bdb28cd6a1a39b28
smhrd <smhrd@211.228.61.200>
2022-07-30 09:14:29 +0900

    지은-점검체크

--- WhereSming/controller/com/wheresming/search/Searching.java
+++ WhereSming/controller/com/wheresming/search/Searching.java
@@ -1,6 +1,5 @@
 package com.wheresming.search;
 import java.io.IOException;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
